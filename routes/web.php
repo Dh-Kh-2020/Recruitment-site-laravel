3117643d4e29cc57336cb8edbbc8af7dbdd9d37b
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-04-10 23:07:52 +0300

    Add: listAll function at AuthCtrl controller

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::post('admin/do-login', [AuthCtrl::class, 'login'])->name('do-admin-login'
 Route::get('admin/register', [AuthCtrl::class, 'showRegister'])->name('admin-register');
 Route::post('admin/do-register', [AuthCtrl::class, 'register'])->name('do-admin-register');
 
-    // ------ Users -------
+    // ------ Admin -------
 Route::get('/admin/dashboard/users-list', function () {
     return view('pages.admin.users-list');
 })->name('all-users');
